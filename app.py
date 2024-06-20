2e3e08b8600bed87b5825218c0b025d4cfee048a
Dau2004 <c.dau@alustudent.com>
2024-06-21 00:54:40 +0200

    commit

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from website import create_app
+
 """
-    This script runs a Flask web application.
+This script runs a Flask web application.
 
-    It imports the `create_app` function from the `website` module and creates an instance of the Flask app.
-    The app is then run in debug mode.
+It imports the `create_app` function from the `website` module and creates an instance of the Flask app.
+The app is then run in debug mode.
 
-    Usage:
-        - Run this script to start the Flask web application.
+Usage:
+    - Run this script to start the Flask web application.
 
-    """
+"""
+
+app = create_app()
+
 if __name__ == "__main__":
-    app = create_app()
     app.run(debug=True)
